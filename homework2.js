0b403c1907b9c857b2aa13b437406e2efa6396ba
GMArtak <43244363+GMArtak@users.noreply.github.com>
2018-09-13 20:16:23 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,17 @@
+// HomeWork 2 Javascript code
+const UserName = "AUA";
+const PassWord = "Baghramyan46";
+const YourAge = 27
+const AgeInputed = 27
+const UsernameInputed = "AUA";
+const PasswordInputed = "Baghramyan26";
+
+
+
+if(UserName === UsernameInputed && PassWord === PasswordInputed && YourAge === AgeInputed + 5 - 5 ){
+	console.log("You are successfully logged in");
+	
+	}
+else{
+	console.log("NO WAY!!!, something went wrong");
+}
